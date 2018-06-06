4a9e58464966a9d8ec2963ebfb7e41422b7eae05
Chuck Liang <chuck_lpc@126.com>
2018-06-06 19:30:13 +0800

    Almost done. Need to add comment.

--- AMT_final/AMT_final.py
+++ AMT_final/AMT_final.py
@@ -69,51 +69,162 @@ def get_rad_fft(rtfunc, r, N, T, with_column_zero=1):
         yf = 1.0/N * np.abs(yf_complex)
         xf = np.linspace(0.0, 1.0/T-1, N)[1:]
         return xf, yf
+
+def get_rot_part(func):
+    def func_out(r, theta):
+        xf, yf = get_rad_fft(func, r, 50, 1.0/50.0, with_column_zero=1)
+        value = yf[0]
+        return value
+    return func_out
+
+def get_nonrot_part(func):
+    rot_part = get_rot_part(func)
+    def func_out(r, theta):
+        value = func(r, theta)-rot_part(r, theta)
+        return value
+    return func_out
+
+def plot_surface(func ,edge_r=15, title_sub='NO TITLE'):
+    X=np.linspace(-edge_r, edge_r, 2*2*edge_r+1)
+    Y=np.linspace(-edge_r, edge_r, 2*2*edge_r+1)
+    #X, Y represent all possible values in the coordinate
+    #using all possible values in linspace object to map all possible points
+    X,Y=np.meshgrid(X,Y)
+    Z = np.zeros((np.shape(X)[0], np.shape(Y)[0]), dtype = np.double)
+    for i in np.arange(0, np.shape(X)[0]):
+        for j in np.arange(0, np.shape(Y)[0]):
+            Z[i, j] = rtheta2xy(func, X[i, j], Y[i, j])
+
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print(title_sub)
+    print('max:')
+    print(Z.max())
+    print('min:')
+    print(Z.min())
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~')
     
+    fig1=plt.figure()#Create a plt object  
+    ax1=Axes3D(fig1, title=title_sub)#Create an Axes object (with 3D cord)  
+    theta_edge = np.linspace(0, 2*np.pi, 200)
+    xr = edge_r*np.cos(theta_edge)
+    yr = edge_r*np.sin(theta_edge)
+    zr = np.zeros(np.size(theta_edge))
+    for i in np.arange(0, np.size(theta_edge)):
+        zr[i] = func(edge_r, theta_edge[i])
+
+    ax1.plot(xr, yr, zr, label='edge')
+    ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm)#construct surface with (x,y,z)
+    ax1.set_xlabel('x label', color='r')  
+    ax1.set_ylabel('y label', color='g')  
+    ax1.set_zlabel('z label', color='b')#adding lable
+    plt.show()#show all plotting objects
+    return
+
+def plot_3d_fft(func, edge_r=15, D=2, N=50, T=1.0/50.0, title_fft='NO TITLE'):
+    fig0 = plt.figure()
+    ax0 = fig0.add_subplot(111, projection='3d', title=title_fft)
+    yticks = np.linspace(0, 15, 15*D+1)
+    for y in yticks:
+        xf, yf = get_rad_fft(func, y, N, T, 0)
+        ax0.bar(xf, yf, zs=y, zdir = 'y', alpha = 0.8)
+    ax0.set_xlabel('X (Digital Frequency)')
+    ax0.set_ylabel('Y (R Position)')
+    ax0.set_zlabel('Z (Amplitude)')
+    #ax0.set_yticks(yticks)#decomment to enable all valid y coordinate
+    return
 
 #next step:
 ##################          (validated)            #######################
 '''
     计算回转部分的三种方式：
     1、直接观察Zernike多项式中与theta无关的项
-    2、进行FFT变换，第0项即为回转部分
+    2、进行FFT变换，第0项即为回转部分(accomplished)
     3、在每个径向位置计算均值
     原多项式与之相减得非回转部分
 '''
 
-#X=np.arange(-15,15,0.5, dtype=np.double)  
-#Y=np.arange(-15,15,0.5, dtype=np.double)#创建了从-2到2，步长为0.1的arange对象  
-X=np.linspace(-15, 15, 61)
-Y=np.linspace(-15, 15, 61)
-#至此X,Y分别表示了取样点的横纵坐标的可能取值  
-#用这两个arange对象中的可能取值一一映射去扩充为所有可能的取样点  
-X,Y=np.meshgrid(X,Y)
-Z = np.zeros((np.shape(X)[0], np.shape(Y)[0]), dtype = np.double)
-for i in np.arange(0, np.shape(X)[0]):
-    for j in np.arange(0, np.shape(Y)[0]):
-        Z[i, j] = rtheta2xy(zfunc, X[i, j], Y[i, j])
+
+# tool start from the edge, theta = 0
+# S = 1000rpm = 1000/60rps
+# take 600 sample points per round
+# 600*1000/60 points per second
+# time interval between two consecutive points = 60/(600*1000) = 0.1ms
+
+theta_var = np.linspace(0, 5*2*np.pi, 5*600+1)[:5*600]
+zfts = np.zeros(5*600)#zfts initalization
+vfts = np.zeros(np.size(zfts)-1)#vfts initalization
+afts = np.zeros(np.size(vfts)-1)#afts initalization
+non_rot_composition = get_nonrot_part(zfunc)
+    
+
+# f = 1mm/min = 1mm/1000rounds = 1/(1000*2*np.pi)mm/rad
+# r's variation based on angle = 1/(1000*2*np.pi)mm/rad
+for i in np.arange(0, np.size(zfts)):
+    r = 15-theta_var[i]*1/(1000*2*np.pi)
+    zfts[i] = non_rot_composition(r, theta_var[i])
+for i in np.arange(0, np.size(vfts)):
+    vfts[i] = (zfts[i+1]-zfts[i])/0.1
+for i in np.arange(0, np.size(afts)):
+    afts[i] = (vfts[i+1]-vfts[i])/0.1
+time = np.arange(0, 4*600*0.1, 0.1)
+plt.subplot(3, 1, 1)
+plt.plot(time, zfts[:np.size(time)], '-')
+plt.title('Zfts')
+plt.ylabel('Zfts/mm')
+plt.xlabel('Time/ms')
+
+plt.subplot(3, 1, 2)
+plt.plot(time, vfts[:np.size(time)], '-')
+plt.title('Vfts')
+plt.ylabel('Vfts/(mm/ms)')
+plt.xlabel('Time/ms')
+
+plt.subplot(3, 1, 3)
+plt.plot(time, afts[:np.size(time)], '-')
+plt.title('Afts')
+plt.ylabel('Afts/(mm/ms^2)')
+plt.xlabel('Time/ms')
+
+plt.show()
+
 
 ########################    PLOTTING SECTION    #################################
-
+# Number of devided times
+#D = 2
 # Number of sample points
-N = 50
+#N = 50
 # sample spacing
-T = 1.0 / 50.0
+#T = 1.0 / 50.0
+plot_3d_fft(zfunc, edge_r=15, D=2, N=50, T=1.0/50.0, title_fft="Amplitude-Frequency Plot")
+plot_surface(zfunc, edge_r=15, title_sub='Curved Surface')
+plot_surface(get_rot_part(zfunc), edge_r=15, title_sub='Rot Component')
+plot_surface(get_nonrot_part(zfunc), edge_r=15, title_sub='NonRot Component')
 
+"""
 fig0 = plt.figure()
 ax0 = fig0.add_subplot(111, projection='3d', title="Amplitude-Frequency Plot")
-yticks = np.linspace(0, 15, 31)
+yticks = np.linspace(0, 15, 15*D+1)
 for y in yticks:
     xf, yf = get_rad_fft(zfunc, y, N, T, 0)
     ax0.bar(xf, yf, zs=y, zdir = 'y', alpha = 0.8)
 ax0.set_xlabel('X (Digital Frequency)')
 ax0.set_ylabel('Y (R Position)')
 ax0.set_zlabel('Z (Amplitude)')
-#ax0.set_yticks(yticks)
+#ax0.set_yticks(yticks)#decomment to enable all valid y coordinate
+"""
+'''
+X=np.linspace(-15, 15, 2*30+1)
+Y=np.linspace(-15, 15, 2*30+1)
+#X, Y represent all possible values in the coordinate
+#using all possible values in linspace object to map all possible points
+X,Y=np.meshgrid(X,Y)
+Z = np.zeros((np.shape(X)[0], np.shape(Y)[0]), dtype = np.double)
+for i in np.arange(0, np.shape(X)[0]):
+    for j in np.arange(0, np.shape(Y)[0]):
+        Z[i, j] = rtheta2xy(zfunc, X[i, j], Y[i, j])
 
-fig1=plt.figure()#创建一个绘图对象  
-ax1=Axes3D(fig1, title="Curved Surface")#用这个绘图对象创建一个Axes对象(有3D坐标)  
-#plt.title("Curved Surface")#总标题
+fig1=plt.figure()#Create a plt object  
+ax1=Axes3D(fig1, title="Curved Surface")#Create an Axes object (with 3D cord)  
 theta_edge = np.linspace(0, 2*np.pi, 200)
 r = 15
 xr = r*np.cos(theta_edge)
@@ -122,8 +233,9 @@ zr = np.zeros(np.size(theta_edge))
 for i in np.arange(0, np.size(theta_edge)):
     zr[i] = zfunc(r, theta_edge[i])
 ax1.plot(xr, yr, zr, label='curve')
-ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm)#用取样点(x,y,z)去构建曲面  
+ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm)#construct surface with (x,y,z)
 ax1.set_xlabel('x label', color='r')  
 ax1.set_ylabel('y label', color='g')  
-ax1.set_zlabel('z label', color='b')#给三个坐标轴注明  
-plt.show()#显示模块中的所有绘图对象
+ax1.set_zlabel('z label', color='b')#adding lable
+plt.show()#show all plotting objects
+'''
